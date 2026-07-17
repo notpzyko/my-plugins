1e768fcfb02865c7690d7d4fff983a8670d8f29b
lex! <85369744+notpzyko@users.noreply.github.com>
2026-07-17 03:52:39 +0100

    Refactor scrollingSlopPx field access and update logic

--- plugins/SwipeActions/src/com/github/razertexz/SwipeActions.kt
+++ plugins/SwipeActions/src/com/github/razertexz/SwipeActions.kt
@@ -1,29 +1,36 @@
 package com.github.razertexz
 
-import androidx.recyclerview.widget.RecyclerView
 import android.content.Context
 import android.util.AttributeSet
-
+import androidx.recyclerview.widget.RecyclerView
+import android.content.res.Resources
 import com.aliucord.annotations.AliucordPlugin
 import com.aliucord.entities.Plugin
-import com.aliucord.patcher.*
-import com.aliucord.utils.DimenUtils
-
+import com.aliucord.patcher.after
 import com.discord.databinding.WidgetChatListBinding
 import com.discord.panels.OverlappingPanelsLayout
 
 @AliucordPlugin(requiresRestart = true)
 class SwipeActions : Plugin() {
+
     override fun start(context: Context) {
-        val scrollingSlopPx = OverlappingPanelsLayout::class.java.getDeclaredField("scrollingSlopPx").apply { isAccessible = true }
-        val newScrollingSlopPx = DimenUtils.dpToPx(8.0f * 3.5f).toFloat()
+        val scrollingSlopField =
+            OverlappingPanelsLayout::class.java.getDeclaredField("scrollingSlopPx").also {
+                it.isAccessible = true
+            }
+        val newScrollingSlopPx = (Resources.getSystem().displayMetrics.density * 8.0f * 3.5f + 0.5f).toInt().toFloat()
+
         patcher.after<OverlappingPanelsLayout>("initialize", AttributeSet::class.java) {
-            scrollingSlopPx.set(this, newScrollingSlopPx)
+            scrollingSlopField.set(it.thisObject, newScrollingSlopPx)
         }
 
         val swipeHelper = SwipeHelper()
-        patcher.after<WidgetChatListBinding>(RecyclerView::class.java, RecyclerView::class.java) {
-            swipeHelper.attachToRecyclerView(it.args[0] as RecyclerView)
+
+        patcher.after<WidgetChatListBinding>(
+            RecyclerView::class.java,
+            RecyclerView::class.java
+        ) { param ->
+            swipeHelper.attachToRecyclerView(param.args[0] as RecyclerView)
         }
     }
 
